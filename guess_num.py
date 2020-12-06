e9110b2ad5858289770f49efb976f644be98ac83
ChiuWT <pocket60223@gmail.com>
2020-12-06 21:57:40 +0800

    add content

--- guess_num.py
+++ guess_num.py
@@ -5,13 +5,17 @@
 
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+    count += 1 # count = count + 1
     guess = input('猜我心裡在想哪個數字？回答：')
     guess = int(guess)
     if guess == r:
         print('你猜對了！')
+        print('這是你猜的第', count, '次')
         break
     elif guess > r:
         print('比答案大')
     elif guess < r:
         print('比答案小')
+    print('這是你猜的第', count, '次')
